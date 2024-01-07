2c4570aedc846b904453c778eb21434784e8e23e
Salina <salina.gurung009@gamil.com>
2024-01-07 17:57:48 +0545

    solving CORS issue

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://connect.facebook.net;
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    connect-src 'self' https://www.facebook.com;
+    frame-src 'self' https://www.facebook.com;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`;
 
-module.exports = nextConfig
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ];
+  },
+};
